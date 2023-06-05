f7e218384f12d59cae148fb0dff1da995c8b05a5
lbihani9 <lokeshbihani99@gmail.com>
2023-06-05 10:42:08 +0530

    adds db connection

--- api/app.js
+++ api/app.js
@@ -1,8 +1,20 @@
-const { ApolloServer } = require('apollo-server');
-const neo4j = require('neo4j-driver');
-const { Neo4jGraphQL } = require('@neo4j/graphql');
-const { startStandaloneServer } = require('@apollo/server/standalone');
+import * as dotenv from "dotenv";
+dotenv.config();
 
-/**
- * import typeDefs, queries, mutations, resolvers from their respective files and combine them here.
- */
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./graphql/type-defs.js";
+import { resolvers } from "./graphql/resolvers.js";
+import initializeDBConnection from "./db.js";
+
+export const driver = initializeDBConnection();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+  listen: { 
+    port: process.env.GRAPHQL_PORT
+  },
+});
+
+console.log(`GraphQL is running at: ${url}`);
