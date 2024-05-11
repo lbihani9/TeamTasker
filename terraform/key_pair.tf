02d24735529ec3a50acee809b560dc369a9099a3
lbihani9 <lokeshbihani99@gmail.com>
2024-05-11 21:54:30 +0530

    Adds terraform scripts

---
+++ terraform/key_pair.tf
@@ -0,0 +1,3 @@
+data "aws_key_pair" "key_pair" {
+  key_pair_id = var.aws_key_pair_id
+}
---
+++ terraform/security_group.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "tt_sg" {
+  name = "tt_security_group"
+  description = "Security group for TeamTasker server"
+
+  ingress {
+    description = "Allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description = "Allow SSH traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  timeouts {
+    delete = "20s"
+  }
+}
